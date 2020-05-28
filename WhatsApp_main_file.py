248a55da5fba6ed8e645bba86528bed34d8836c4
Ashutosh Tiwari <ashu.atiwa@gmail.com>
2020-05-28 11:28:28 +0530

    Update WhatsApp_main_file.py

--- WhatsApp_main_file.py
+++ WhatsApp_main_file.py
@@ -9,18 +9,18 @@ with open('sample.csv',"r") as f:
     for row in csv_reader:
         l.append(row[0])
 
-driver=webdriver.Chrome("E:\\Drivers\\chromedriver.exe")   # Selenium chromedriver path
+driver=webdriver.Chrome("E:\Drivers\chromedriver.exe")   # Selenium chromedriver path
 driver.get("https://web.whatsapp.com/")
 
-input("Enter any keyword after scanning QR code ")  
+input("Enter any keyword after scanning QR code.\n ")  
 
 for name in l:
     user = driver.find_element_by_xpath("//span[@title='{}']".format(name))
     user.click()
 
     text_box = driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-    for _ in range(2):
-        text_box.send_keys("Hii")
+    text_box.send_keys("Hiiii")
 
-    button = driver.find_element_by_class_name("_3M-N-")
+    button = driver.find_element_by_class_name("_35EW6")
     button.click()
+
